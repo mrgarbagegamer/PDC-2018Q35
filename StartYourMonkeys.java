085756c3ba034390f0e04f136e80db66ab9e17b7
Leao Fernandes <lcf0109@gmail.com>
2025-04-01 13:44:43 -0400

    fixed default values for numThread and numClicks...

--- StartYourMonkeys.java
+++ StartYourMonkeys.java
@@ -18,20 +18,18 @@ public class StartYourMonkeys {
         int defaultNumClicks  = 9;
         int defaultNumThreads = 10;
 
-        int numClicks  = 0;
-        int numThreads = 10;
+        int numClicks  = defaultNumClicks;
+        int numThreads = defaultNumThreads;
 
         // retrieve the passed in number of clicks if any or set a default value
         try {
-            numClicks  = Integer.parseInt(args[1]);
-            numThreads = Integer.parseInt(args[2]);
+            numClicks  = Integer.parseInt(args[0]);
+            numThreads = Integer.parseInt(args[1]);
         } catch (Exception e) {
             numClicks  = defaultNumClicks;
             numThreads = defaultNumThreads;
         }
 
-        // the initial grid for the problem we are looking to solve
-        Grid puzzleGrid = new Grid22();
 
         // generate the list of possible clicks for our grid
         List<Click> possibleClicks = new ArrayList<>();
@@ -48,7 +46,9 @@ public class StartYourMonkeys {
         TestClickCombination[] monkeys = new TestClickCombination[numThreads];
 
         for(int i=0; i < numThreads; i++){
+            Grid puzzleGrid = new Grid22();
             String threadName = String.format("Monkey-%d", i);
+
             monkeys[i] = new TestClickCombination(threadName, combinationQueue, puzzleGrid);
             monkeys[i].start();
         }
@@ -62,7 +62,19 @@ public class StartYourMonkeys {
             }
         }
 
-        System.out.printf("%s - Found the solution as the following click combination:\n[%s]\n", combinationQueue.getWinningMonkey(), combinationQueue.getClicksCombination());
+        List<Click> winningCombination = combinationQueue.getClicksCombination();
+
+        System.out.printf("%s - Found the solution as the following click combination:\n[%s]\n", combinationQueue.getWinningMonkey(), winningCombination);
+
+        // create a new grid and test out the winning combination
+        Grid puzzleGrid = new Grid22();
+        boolean solved = false;
+        for (int i = 0; (i < winningCombination.size()) && (!solved); i++) {
+            Click click = winningCombination.get(i);
+
+            puzzleGrid.click(click.row, click.col);
+            solved = puzzleGrid.isSolved();
+        }        
 
         puzzleGrid.printGrid();
     }

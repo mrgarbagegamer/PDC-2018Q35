f5083ff3f075a61b3a1ea61663dccac7b6b7090a
Apollo Fernandes <20287746+mrgarbagegamer@users.noreply.github.com>
2025-06-28 22:06:30 -0400

    Initialize a reference array and use arraycopy() in initialize() to minimize iteration operations and get rid of Arrays.fill() calls

--- src/main/java/com/github/mrgarbagegamer/Grid13.java
+++ src/main/java/com/github/mrgarbagegamer/Grid13.java
@@ -1,28 +1,33 @@
 package com.github.mrgarbagegamer;
 
-import java.util.Arrays;
 public class Grid13 extends Grid 
 {
-    void initialize() 
-    {
-        // Initialize for Q13/Kermit
-        
-        // set all cells to false
-        Arrays.fill(trueCells, false);
+    private static final boolean[] reference;
 
+    static
+    {
+        reference = new boolean[NUM_CELLS];
         for (int row = 2; row <= 4; row += 2)
         {
             for (int col = 1; col <= 14; col++)
             {
                 int cell = row * 100 + col;
-                // Add the true cells to the trueCells BitSet
-                this.trueCells[packedToIndex(cell)] = true;
+                // Add the true cells to the reference BitSet
+                reference[packedToIndex(cell)] = true;
             }
         }
 
-        trueCells[47] = true; // Set cell 300 (pre-computed index)
+        reference[47] = true; // Set cell 300 (pre-computed index)
 
-        trueCells[61] = true; // Set cell 314 (pre-computed index)
+        reference[61] = true; // Set cell 314 (pre-computed index)
+    }
+    
+    void initialize() 
+    {
+        // Initialize for Q13/Kermit
+        
+        // set all cells to their initial state
+        System.arraycopy(reference, 0, trueCells, 0, NUM_CELLS);
 
         firstTrueCell = 201; // Set the first true cell to 201 (row 2, col 1)
         trueCellsCount = 30;

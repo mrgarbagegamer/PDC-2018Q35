1f208d586e4df29e6fb4f77fb471e2f073664597
Apollo Fernandes <20287746+mrgarbagegamer@users.noreply.github.com>
2026-06-02 12:43:14 -0400

    Write a unit test for the null check in QueueListValidatorTest.validateIntegrity()

--- core/src/test/java/com/github/mrgarbagegamer/queues/QueueListValidatorTest.java
+++ core/src/test/java/com/github/mrgarbagegamer/queues/QueueListValidatorTest.java
@@ -1,5 +1,8 @@
 package com.github.mrgarbagegamer.queues;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -8,7 +11,13 @@ public class QueueListValidatorTest {
 
     // validateIntegrity() tests:
 
-    // TODO: Write tests for the null check in validateIntegrity()
+    @Test
+    void givenNullGroup_whenValidateIntegrity_thenThrowNullPointerException() {
+        assertThatThrownBy(() -> QueueListValidator.validateIntegrity(null))
+                .isInstanceOf(NullPointerException.class)
+                .hasMessageContaining("group must not be null");
+    }
+
     // TODO: Write tests for the emptiness check in validateIntegrity()
     // TODO: Write tests for the duplicate check in validateIntegrity()
 

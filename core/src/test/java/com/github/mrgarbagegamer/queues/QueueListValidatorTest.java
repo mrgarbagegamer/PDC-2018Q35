95caa2618ef17a597a339785b32e605d55b083dd
Apollo Fernandes <20287746+mrgarbagegamer@users.noreply.github.com>
2026-06-02 12:36:39 -0400

    Create QueueListValidatorTest to house unit tests for QueueListValidator, and write TODOs for each appropriate thing to test in the class

---
+++ core/src/test/java/com/github/mrgarbagegamer/queues/QueueListValidatorTest.java
@@ -0,0 +1,19 @@
+package com.github.mrgarbagegamer.queues;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class QueueListValidatorTest {
+
+    // validateIntegrity() tests:
+
+    // TODO: Write tests for the null check in validateIntegrity()
+    // TODO: Write tests for the emptiness check in validateIntegrity()
+    // TODO: Write tests for the duplicate check in validateIntegrity()
+
+    // validateNoOverlap() tests:
+
+    // TODO: Write tests for the null checks in validateNoOverlap()
+    // TODO: Write tests for the overlap check in validateNoOverlap()
+}

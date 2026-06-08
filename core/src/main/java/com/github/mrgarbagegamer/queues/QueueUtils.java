159bc6297ceb86058664d17c17e6be36fb96b275
Apollo Fernandes <20287746+mrgarbagegamer@users.noreply.github.com>
2026-06-08 13:47:20 -0400

    Create new utility methods in QueueUtils to generate immutable lists

--- core/src/main/java/com/github/mrgarbagegamer/queues/QueueUtils.java
+++ core/src/main/java/com/github/mrgarbagegamer/queues/QueueUtils.java
@@ -2,8 +2,13 @@ package com.github.mrgarbagegamer.queues;
 
 import static com.github.mrgarbagegamer.internal.ValidationUtils.mustBePositive;
 import static com.github.mrgarbagegamer.internal.ValidationUtils.utilityClassError;
+import static java.util.stream.Collectors.toUnmodifiableList;
 
+import java.util.List;
 import java.util.concurrent.BlockingQueue;
+import java.util.function.IntFunction;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 import org.jctools.queues.MessagePassingQueue;
 
@@ -129,5 +134,16 @@ public final class QueueUtils {
         } else {
             return 1 << (32 - Integer.numberOfLeadingZeros(n - 1));
         }
+    }
+
+    // List creation utility methods:
+
+    static <Q> List<Q> newImmutableQueueList(int listSize, Supplier<? extends Q> elementSupplier) {
+        return Stream.generate(elementSupplier).limit(listSize).collect(toUnmodifiableList());
+    }
+
+    static <Q> List<Q> newBoundedImmutableQueueList(int listSize, int queueCapacity,
+            IntFunction<? extends Q> elementConstructor) {
+        return newImmutableQueueList(listSize, () -> elementConstructor.apply(queueCapacity));
     }
 }

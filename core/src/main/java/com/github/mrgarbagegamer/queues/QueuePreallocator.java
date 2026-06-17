a1988ae7d79cece95b7eb689739c2dca8b91f98f
Apollo Fernandes <20287746+mrgarbagegamer@users.noreply.github.com>
2026-06-16 22:27:07 -0400

    Refactor QueuePreallocator.tryOffer() to fold exception formatting logic into the preallocate() method

--- core/src/main/java/com/github/mrgarbagegamer/queues/QueuePreallocator.java
+++ core/src/main/java/com/github/mrgarbagegamer/queues/QueuePreallocator.java
@@ -1,5 +1,9 @@
 package com.github.mrgarbagegamer.queues;
 
+import static com.github.mrgarbagegamer.internal.ValidationUtils.copyOfNonNullList;
+import static com.github.mrgarbagegamer.internal.ValidationUtils.mustBePositive;
+import static com.github.mrgarbagegamer.internal.ValidationUtils.mustNotBeEmpty;
+import static com.github.mrgarbagegamer.internal.ValidationUtils.mustNotBeNull;
 import static com.github.mrgarbagegamer.internal.ValidationUtils.utilityClassError;
 
 import java.util.List;
@@ -10,22 +14,32 @@ import com.github.mrgarbagegamer.internal.ExcludeFromGeneratedCoverage;
 
 final class QueuePreallocator {
     private static final String REJECTION_REASON_TEMPLATE = "mtgQueue at index %d rejected batch"
-            + "%d during preallocation";
+            + " %d during preallocation";
 
     @ExcludeFromGeneratedCoverage
     private QueuePreallocator() { utilityClassError("QueuePreallocator"); }
 
     static void preallocate(List<? extends QueueWrapper<?>> mtgQueues,
             SolverConfiguration solverConfig, int batchesPerQueue) {
+        final List<QueueWrapper<?>> nonNullQueues = copyOfNonNullList(mtgQueues, "mtgQueues");
+        mustNotBeEmpty(nonNullQueues, "mtgQueues");
+        mustNotBeNull(solverConfig, "solverConfig");
+        mustBePositive(batchesPerQueue, "batchesPerQueue");
 
         // Check that all queues are empty and have sufficient capacity for the preallocation:
-        verifyPreconditions(mtgQueues, batchesPerQueue);
+        verifyPreconditions(nonNullQueues, batchesPerQueue);
 
         // Preallocate:
-        for (int i = 0; i < mtgQueues.size(); i++) {
-            final var queue = mtgQueues.get(i);
+        for (int i = 0; i < nonNullQueues.size(); i++) {
+            final var queue = nonNullQueues.get(i);
             for (int j = 0; j < batchesPerQueue; j++) {
-                tryOffer(solverConfig, i, queue, j);
+                if (!tryOffer(queue, solverConfig)) {
+                    if (queue.boundedness().isBounded()) {
+                        fail("bounded " + REJECTION_REASON_TEMPLATE.formatted(i, j));
+                    } else {
+                        fail("unbounded " + REJECTION_REASON_TEMPLATE.formatted(i, j));
+                    }
+                }
             }
         }
     }
@@ -43,15 +57,8 @@ final class QueuePreallocator {
         }
     }
 
-    private static void tryOffer(SolverConfiguration solverConfig, int queueIndex,
-            QueueWrapper<?> queue, int batchNumber) {
-        if (!queue.offer(new WorkBatch(solverConfig))) {
-            if (queue.boundedness().isBounded()) {
-                fail("bounded " + REJECTION_REASON_TEMPLATE.formatted(queueIndex, batchNumber));
-            } else {
-                fail("unbounded " + REJECTION_REASON_TEMPLATE.formatted(queueIndex, batchNumber));
-            }
-        }
+    private static boolean tryOffer(QueueWrapper<?> queue, SolverConfiguration solverConfig) {
+        return queue.offer(new WorkBatch(solverConfig));
     }
 
     private static void fail(String reason) {

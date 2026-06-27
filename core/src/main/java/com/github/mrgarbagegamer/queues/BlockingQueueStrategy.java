df8dd220d3b0359f0f77cbaf08bdea979d1756d7
Apollo Fernandes <20287746+mrgarbagegamer@users.noreply.github.com>
2026-06-27 12:02:20 -0400

    Update the call-sites of BlockingQueueWrappers.java and JCToolsWrappers.java to use the new wrappers and delete the now obsolete files

--- core/src/main/java/com/github/mrgarbagegamer/queues/BlockingQueueStrategy.java
+++ core/src/main/java/com/github/mrgarbagegamer/queues/BlockingQueueStrategy.java
@@ -2,7 +2,7 @@ package com.github.mrgarbagegamer.queues;
 
 import static com.github.mrgarbagegamer.internal.ValidationUtils.mustBeSet;
 import static com.github.mrgarbagegamer.internal.ValidationUtils.mustNotBeNull;
-import static com.github.mrgarbagegamer.queues.BlockingQueueWrappers.wrapAll;
+import static com.github.mrgarbagegamer.queues.QueueWrappers.BlockingQueueWrappers.wrapList;
 import static com.github.mrgarbagegamer.queues.QueueSelectors.exclusiveBlocking;
 import static com.github.mrgarbagegamer.queues.QueueSelectors.preferredBlocking;
 import static com.github.mrgarbagegamer.queues.QueueUtils.newBoundedImmutableQueueList;
@@ -194,8 +194,8 @@ public class BlockingQueueStrategy<G extends BlockingQueue<WorkBatch>, M extends
         @Override
         public BlockingQueueStrategy<G, M> build() {
             // 1. Wrap the queues:
-            final var wrappedGtmQueues = wrapAll(this.gtmQueues);
-            final var wrappedMtgQueues = wrapAll(this.mtgQueues);
+            final var wrappedGtmQueues = wrapList(this.gtmQueues);
+            final var wrappedMtgQueues = wrapList(this.mtgQueues);
 
             // 2. Build and validate the context:
             final var context = QueueValidationContext.builder(wrappedGtmQueues, wrappedMtgQueues)
--- core/src/main/java/com/github/mrgarbagegamer/queues/JCToolsQueueStrategy.java
+++ core/src/main/java/com/github/mrgarbagegamer/queues/JCToolsQueueStrategy.java
@@ -2,7 +2,7 @@ package com.github.mrgarbagegamer.queues;
 
 import static com.github.mrgarbagegamer.internal.ValidationUtils.mustBeSet;
 import static com.github.mrgarbagegamer.internal.ValidationUtils.mustNotBeNull;
-import static com.github.mrgarbagegamer.queues.JCToolsWrappers.wrapAll;
+import static com.github.mrgarbagegamer.queues.QueueWrappers.JCToolsWrappers.wrapList;
 import static com.github.mrgarbagegamer.queues.QueueSelectors.biasedSequentialJCTools;
 import static com.github.mrgarbagegamer.queues.QueueSelectors.exclusiveJCTools;
 import static com.github.mrgarbagegamer.queues.QueueSelectors.preferredJCTools;
@@ -205,8 +205,8 @@ public class JCToolsQueueStrategy<G extends MessagePassingQueue<WorkBatch>, M ex
         @Override
         public JCToolsQueueStrategy<G, M> build() {
             // 1. Wrap the queues:
-            final var wrappedGtmQueues = wrapAll(this.gtmQueues);
-            final var wrappedMtgQueues = wrapAll(this.mtgQueues);
+            final var wrappedGtmQueues = wrapList(this.gtmQueues);
+            final var wrappedMtgQueues = wrapList(this.mtgQueues);
 
             // 2. Build and validate the context:
             final var context = QueueValidationContext.builder(wrappedGtmQueues, wrappedMtgQueues)

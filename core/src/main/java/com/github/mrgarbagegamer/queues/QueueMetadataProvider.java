07290b3c36a1c4ee084de3361712c28e0ee1eaa8
Apollo Fernandes <20287746+mrgarbagegamer@users.noreply.github.com>
2026-07-01 21:21:59 -0400

    Delete the QueueMetadataProvider interface

--- core/src/main/java/com/github/mrgarbagegamer/queues/QueueMetadataProvider.java
+++
@@ -1,3 +0,0 @@
-package com.github.mrgarbagegamer.queues;
-
-public interface QueueMetadataProvider {}
--- core/src/main/java/com/github/mrgarbagegamer/queues/QueueWrapper.java
+++ core/src/main/java/com/github/mrgarbagegamer/queues/QueueWrapper.java
@@ -8,7 +8,7 @@ import java.util.List;
 import com.github.mrgarbagegamer.WorkBatch;
 
 // TODO: Remove Javadocs, since the interface isn't public and is used internally.
-interface QueueWrapper<Q> extends QueueMetadataProvider {
+interface QueueWrapper<Q> {
     /**
      * Unwraps the underlying queue from this wrapper.
      * 

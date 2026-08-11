30e80dab133e4c4008243c881210e297a2fdb13b
Apollo Fernandes <20287746+mrgarbagegamer@users.noreply.github.com>
2026-08-10 23:11:16 -0400

    Delete DefaultGeneratorContext.java and GeneratorFactory.java

--- core/src/main/java/com/github/mrgarbagegamer/GeneratorFactory.java
+++
@@ -1,8 +0,0 @@
-package com.github.mrgarbagegamer;
-
-import java.util.concurrent.ForkJoinPool.ForkJoinWorkerThreadFactory;
-
-// TODO: Delete this interface
-@Deprecated
-@FunctionalInterface
-interface GeneratorFactory extends ForkJoinWorkerThreadFactory {}
